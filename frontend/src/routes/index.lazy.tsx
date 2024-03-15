069b2b947e50a3df60ae2be30f44477e65c22ebc
Ruben Esteves <ruben38esteves@gmail.com>
2024-03-15 15:45:01 +0000

    now supports file dragging

--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -3,6 +3,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ChangeEvent, useContext } from "react";
 import { GRPCContext } from "@/lib/context";
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
@@ -27,15 +29,39 @@ function Index() {
     console.log(a.response);
   }
 
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach(async (file: File) => {
+      console.log("batatinhas")
+
+      const value_read: ArrayBuffer = await file.arrayBuffer();
+
+      const a = await grpc.client.uploadFile({
+        file: new Uint8Array(value_read),
+      });
+      console.log(a.response);
+    })
+    
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
+  
   return (
-    <div className="flex flex-col h-svh gap-1.5 justify-center items-center">
-      <Label htmlFor="file_upload">Upload .fits file</Label>
-      <Input
-        className="w-75"
-        id="file_upload"
-        type="file"
-        onChange={handleFileUpload}
-      />
+    <div className="flex flex-col h-svh w-svh gap-1.5 justify-center items-center px-40">
+      <div {...getRootProps()} className="w-screen bg-gray-200 rounded-sm border-2 border-black border-dashed cursor-pointer">
+        <input {...getInputProps()} />
+        <p className="mt-3 text-center">Click or drag a file</p>
+        <p className="text-5xl text-center mb-3">+</p>
+      </div>
     </div>
   );
 }
+/*
+<div className="flex flex-col h-svh gap-1.5 justify-center items-center">
+  <Label htmlFor="file_upload">Upload .fits file</Label>
+  <Input
+    className="w-75"
+    id="file_upload"
+    type="file"
+    onChange={handleFileUpload}
+  />
+</div>
+*/
